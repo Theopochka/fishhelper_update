a16fc4e5275aacc7b8abb56effd0f7ccee7184a0
Theopochka <theopka0@gmail.com>
2025-01-04 20:31:29 +0200

    Add files via upload

--- fishhelper.lua
+++ fishhelper.lua
@@ -626,6 +626,61 @@ end
 function msg(message)
     sampAddChatMessage('[fishHelper]: {FFFFFF}'..message, 0x2568BE)
 end
+function sendFrontendClick(interfaceid, id, subid, json)
+    local bs = raknetNewBitStream()
+    raknetBitStreamWriteInt8(bs, 220)
+    raknetBitStreamWriteInt8(bs, 63)
+    raknetBitStreamWriteInt8(bs, interfaceid)
+    raknetBitStreamWriteInt32(bs, id)
+    raknetBitStreamWriteInt32(bs, subid)
+    raknetBitStreamWriteInt32(bs, #json)
+    raknetBitStreamWriteString(bs, json)
+    raknetSendBitStreamEx(bs, 1, 10, 1)
+    raknetDeleteBitStream(bs)
+end
+
+function sendInterfaceLoaded(interfaceid, bool)
+    local bs = raknetNewBitStream()
+    raknetBitStreamWriteInt8(bs, 220)
+    raknetBitStreamWriteInt8(bs, 66)
+    raknetBitStreamWriteInt8(bs, interfaceid)
+    raknetBitStreamWriteBool(bs, bool)
+    raknetSendBitStreamEx(bs, 1, 10, 1)
+    raknetDeleteBitStream(bs)
+end
+
+addEventHandler('onReceivePacket', function(id, bs)
+    if not ini.autocaptcha.enabled then return end
+
+    if id == 220 then
+        raknetBitStreamIgnoreBits(bs, 8)
+        local type = raknetBitStreamReadInt8(bs)
+        
+        if type == 84 then
+            local interfaceid = raknetBitStreamReadInt8(bs)
+            local subid = raknetBitStreamReadInt8(bs)
+            --local len = raknetBitStreamReadInt32(bs)
+            --local json = raknetBitStreamReadString(bs, len)
+            
+            if interfaceid == 81 then
+                lua_thread.create(function()
+                    wait(ini.autocaptcha.delay)
+                    sendFrontendClick(81, 0, 0, "")
+                end)
+                return false
+            end
+            
+        elseif type == 62 then
+            local interfaceid = raknetBitStreamReadInt8(bs)
+            local toggle = raknetBitStreamReadBool(bs)
+            
+            if interfaceid == 81 then
+                sendInterfaceLoaded(81, toggle)
+                return false
+            end
+        end
+    end
+end)
 --- декор
 function DarkTheme()
     imgui.SwitchContext()
